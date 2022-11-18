ac85aa892cfecd823960892f12bbfce86e6a4cff
István Hajdu <istvan.hajdu@mxcsoftware.it>
2022-11-19 00:52:05 +0100

    Add diagram component

--- roadToImportal/src/app/app-routing.module.ts
+++ roadToImportal/src/app/app-routing.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {DiagramComponent} from "./diagram/component/diagram.component";
 
-const routes: Routes = [];
+const routes: Routes = [{path: '',component: DiagramComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ roadToImportal/src/app/diagram/diagram.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {DiagramComponent} from "./component/diagram.component";
+
+
+
+@NgModule({
+  declarations: [DiagramComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class DiagramModule { }
